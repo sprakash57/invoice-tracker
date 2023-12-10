6dc3f8c651ac269719825c9e775f33d63dee1a11
Sunny Prakash <sunny.prakashgm@gmail.com>
2023-12-10 20:59:55 +0100

    feat: add revenue, customer and invoice widget on dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -41,11 +41,10 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices = data.rows.map((invoice) => ({
+    return data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
-    return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch the latest invoices.');
