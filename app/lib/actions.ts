bb41dae5f0afc205531ba95687f9a90631fed7ea
Sunny Prakash <sunny.prakashgm@gmail.com>
2023-12-28 22:19:15 +0100

    feat: add authentication and metadata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
+import { signIn } from '@/auth';
 import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
@@ -104,5 +106,24 @@ export const deleteInvoice = async (id: string) => {
     return { message: 'Deleted Invoice' };
   } catch (error) {
     return { message: 'DB Error: Failed to delete invoice' };
+  }
+};
+
+export const authenticate = async (
+  prevState: string | undefined,
+  formData: FormData,
+) => {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
   }
 };
